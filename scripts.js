fd55d5f83a1bcf4bca972678979116f9eaaf5cfe
AnetteHdz <113574673+AnetteHdz@users.noreply.github.com>
2024-09-12 14:09:04 -0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,4 @@
+function actualizarMonto(valor) {
+    document.getElementById("monto-valor").textContent = "$" + valor;
+    document.getElementById("monto-seleccionado").textContent = "$" + valor;
+}
